9f89b6c87790104edcc67b72debb426eeefae9c6
bugai <bosco.bugai@gmail.com>
2024-05-16 00:29:56 +0300

    Добавьте файлы проекта.

---
+++ ClassLibraryCourseWork/Model/Diploma.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassLibraryCourseWork.Model
+{
+    public class Diploma: Work
+    {
+        public string QualificationLevel { get; set; }
+    }
+}
---
+++ ClassLibraryCourseWork/Repo/WorkContext.cs
@@ -0,0 +1,20 @@
+﻿using ClassLibraryCourseWork.Model;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassLibraryCourseWork.Repo
+{
+    public class WorkContext : DbContext
+    {
+        public DbSet<Work> Works { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseNpgsql("Host=localhost;Database=workDb;Username=postgres;Password=postgres");
+        }
+    }
+}
